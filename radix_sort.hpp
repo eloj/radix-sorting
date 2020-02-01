05c7340479b07b1da4300da21ec7ab8535913c54
Eddy Jansson <eddy@klopper.net>
2020-02-01 21:08:47 +0100

    More "day after" C++ clean-up.
    Be smarter about type deductions.
    
    Make it so that we can provide our own kdf while having
    the size of the histogram array correctly deduced from
    the return-type of the kdf lambda function.

--- radix_sort.hpp
+++ radix_sort.hpp
@@ -6,15 +6,16 @@
 
 */
 #include <array>
+#include <type_traits>
 #include <cinttypes>
 #include <cstring> // for std::memcpy
 
 #define RESTRICT __restrict__
 
-template<typename T, typename KeyFunc, typename Hist>
+template<typename T, typename KeyFunc, typename Hist, typename KeyType=typename std::result_of<KeyFunc(T)>::type>
 T* rs_sort_main(T* RESTRICT src, T* RESTRICT aux, size_t n, Hist& histogram, KeyFunc && kf) {
 	typedef typename Hist::value_type HVT;
-	static_assert(sizeof(T) <= 8, "Sort key must be 64-bits or less");
+	static_assert(sizeof(KeyType) <= 8, "Sort key must be 64-bits or less");
 
 	if (n < 2)
 		return src;
@@ -83,68 +84,59 @@ constexpr typename std::make_unsigned<T>::type highbit(void) {
 	return a;
 }
 
-template<typename T, typename KT=T, typename HT>
+template<typename T, typename KeyFunc, int passes = sizeof(typename std::result_of<KeyFunc(T)>::type)>
+T* radix_sort(T* RESTRICT src, T* RESTRICT aux, size_t n, KeyFunc && kf) {
+	if (n < 256) {
+		std::array<uint8_t,256*passes> histogram{0};
+		return rs_sort_main(src, aux, n, histogram, kf);
+	} else if (n < (1ULL << 16ULL)) {
+		std::array<uint16_t,256*passes> histogram{0};
+		return rs_sort_main(src, aux, n, histogram, kf);
+	} else if (n < (1ULL << 32ULL)) {
+		std::array<uint32_t,256*passes> histogram{0};
+		return rs_sort_main(src, aux, n, histogram, kf);
+	} else {
+		std::array<uint64_t,256*passes> histogram{0};
+		return rs_sort_main(src, aux, n, histogram, kf);
+	}
+}
+
+template<typename T, typename KT=T>
 std::enable_if_t<std::is_integral_v<T> && std::is_unsigned_v<T> && !std::is_same_v<T,bool>, T*>
-rs_select_kf(T* RESTRICT src, T* RESTRICT aux, size_t n, HT& histogram) {
+radix_sort(T* RESTRICT src, T* RESTRICT aux, size_t n) {
 	auto kf_unsigned = [](const T& entry) -> KT {
 			return entry;
 	};
-	return rs_sort_main(src, aux, n, histogram, kf_unsigned);
+	return radix_sort(src, aux, n, kf_unsigned);
 }
 
-template<typename T, typename KT=std::make_unsigned<T>, typename HT>
+template<typename T, typename KT=std::make_unsigned<T>>
 std::enable_if_t<std::is_integral_v<T> && std::is_signed_v<T> && !std::is_same_v<T,bool>, T*>
-rs_select_kf(T* RESTRICT src, T* RESTRICT aux, size_t n, HT& histogram) {
+radix_sort(T* RESTRICT src, T* RESTRICT aux, size_t n) {
 	auto kf_signed = [](const T& entry) -> typename KT::type {
 		return entry ^ highbit<T>();
 	};
-	return rs_sort_main(src, aux, n, histogram, kf_signed);
+	return radix_sort(src, aux, n, kf_signed);
 }
 
-template<typename T, typename KT=uint32_t, typename HT>
+template<typename T, typename KT=uint32_t>
 std::enable_if_t<std::is_same_v<T,float>, T*>
-rs_select_kf(T* RESTRICT src, T* RESTRICT aux, size_t n, HT& histogram) {
+radix_sort(T* RESTRICT src, T* RESTRICT aux, size_t n) {
 	auto kf_float = [](const T& entry) -> KT {
 		KT local;
 		std::memcpy(&local, &entry, sizeof(local));
 		return local ^ (-(local >> 31UL) | (1UL << 31UL));
 	};
-	return rs_sort_main(src, aux, n, histogram, kf_float);
+	return radix_sort(src, aux, n, kf_float);
 }
 
-template<typename T, typename KT=uint64_t, typename HT>
+template<typename T, typename KT=uint64_t>
 std::enable_if_t<std::is_same_v<T,double>, T*>
-rs_select_kf(T* RESTRICT src, T* RESTRICT aux, size_t n, HT& histogram) {
+radix_sort(T* RESTRICT src, T* RESTRICT aux, size_t n) {
 	auto kf_double = [](const T& entry) -> KT {
 		KT local;
 		std::memcpy(&local, &entry, sizeof(local));
 		return local ^ (-(local >> 63ULL) | (1ULL << 63ULL));
 	};
-	return rs_sort_main(src, aux, n, histogram, kf_double);
+	return radix_sort(src, aux, n, kf_double);
 }
-
-template<typename T>
-T* radix_sort(T* RESTRICT src, T* RESTRICT aux, size_t n) {
-	if (n < 256) {
-		std::array<uint8_t,256*sizeof(T)> histogram{0};
-		return rs_select_kf(src, aux, n, histogram);
-	} else if (n < (1ULL << 16ULL)) {
-		std::array<uint16_t,256*sizeof(T)> histogram{0};
-		return rs_select_kf(src, aux, n, histogram);
-	} else if (n < (1ULL << 32ULL)) {
-		std::array<uint32_t,256*sizeof(T)> histogram{0};
-		return rs_select_kf(src, aux, n, histogram);
-	} else {
-		std::array<uint64_t,256*sizeof(T)> histogram{0};
-		return rs_select_kf(src, aux, n, histogram);
-	}
-}
-
-/*
-template<typename T, typename KeyFunc>
-T* radix_sort(T* RESTRICT src, T* RESTRICT aux, size_t n, KeyFunc && kf) {
-	std::array<size_t,256*sizeof(T)> histogram{0};
-
-	return rs_sort_main(src, aux, histogram, n, kf);
-}
-*/
